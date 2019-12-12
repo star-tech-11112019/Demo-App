258d2a4b81a063d687d9e0f8f792e8ff4bfb2ce0
Mohit Kumar. Awasthi <Mohit.Awasthi@srmtechsol.com>
2019-12-12 10:54:28 +0530

    fixing add products

--- src/app/members/product/product.page.ts
+++ src/app/members/product/product.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-product',
@@ -8,9 +9,18 @@ import { Router } from '@angular/router';
 })
 export class ProductPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private storage: Storage) { }
 
   ngOnInit() {
+    console.log("sss ##############");
+  }
+
+  get products() {
+    let p:any;
+    // this.storage.get('poducts').then((val) => {
+    //   console.log('Your age is', JSON.parse(val));
+    // });
+    return ;
   }
 
 }
